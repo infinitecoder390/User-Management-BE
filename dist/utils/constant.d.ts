dd2f6a31c01f088122cb866e9490c46c8dbc6f59
infinitecoder390 <infinitecoder390@gmail.com>
2024-03-15 02:39:53 +0530

    commit for env

--- dist/utils/constant.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const saltOrRounds = 10;
-export declare const max_image_size = 1048576;
--- dist/utils/constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.max_image_size = exports.saltOrRounds = void 0;
-exports.saltOrRounds = 10;
-exports.max_image_size = 1048576;
-//# sourceMappingURL=constant.js.map
